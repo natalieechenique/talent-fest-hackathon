56b733b32d6c451d8ee25b83ebb4a6131b188ff8
natalieechenique <natalie.terapias@gmail.com>
2020-09-30 20:16:03 -0300

    mmergin

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
         return (
                 <div className='footer'>
-                        <p className='footer-text'>EY © 2020</p>
+                        <p className='footer-text'>EY-Temenos-WorkUp-Laboratoria © 2020</p>
                 </div>
         );
 }
